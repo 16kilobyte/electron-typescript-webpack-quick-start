3f7474192ff73b0524f1aed29e2df1315df093e7
Kator James <kator95@gmail.com>
2019-12-03 13:12:02 +0100

    :tada: Initial commit :tada:

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from "react";
+
+class App extends PureComponent {
+  public render() {
+    return (
+      <div>
+        Hello Kator
+      </div>
+    );
+  }
+}
+
+export default App;
